7cd26894de4c31e90dbc6b8f28ebbd3ccc28ed5a
ahlam10 <ahlam_11a@hotmail.com>
2023-12-20 15:09:12 +0300

    Initial Commit

---
+++ NC2_teme5/NC2_teme5App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_teme5App.swift
+//  NC2_teme5
+//
+//  Created by Ahlam ALshehri on 07/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NC2_teme5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
